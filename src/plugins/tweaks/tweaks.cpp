b89764366bde2271187838da47eb77b1d6c40c0b
virusman <virusman@virusman.ru>
2012-06-12 22:52:04 +0000

    Tweaks 0.2.2: added Beams

--- src/plugins/tweaks/tweaks.cpp
+++ src/plugins/tweaks/tweaks.cpp
@@ -11,6 +11,7 @@ FILE *logFile;
 char logFileName[] = "logs/nwncx_tweaks.txt";
 
 void (*CNWCItem__LoadVisualEffect_origmid)();
+void (*CNWCVisualEffectOnObject__LoadBeam_origmid)();
 
 void EnableWrite (unsigned long location)
 {
@@ -46,8 +47,10 @@ int __stdcall CNWCItem__LoadVisualEffect_Hook(int nType, CExoString &sFxName)
 		
 		fprintf(logFile, "sFxName: %s\n", sFxName.Text);
 		fflush(logFile);
+		return true;
 	}
-	return true;
+	else
+		return false;
 }
 
 unsigned int CNWCItem__LoadVisualEffect_eax;
@@ -71,6 +74,46 @@ void __declspec( naked ) CNWCItem__LoadVisualEffect_hookmid()
 	}
 }
 
+int __stdcall CNWCVisualEffectOnObject__LoadBeam_Hook(int nType, CExoString &sFxName)
+{
+	fprintf(logFile, "CNWCVisualEffectOnObject__LoadBeam_Hook: %d\n", nType);
+
+	CExoString sModel;
+	C2DA tVisualFX("vfx_beams", 0);
+	tVisualFX.Load2DArray();
+	tVisualFX.GetCExoStringEntry(nType, "Model", &sModel);
+	if(sModel.Length > 0){
+		sFxName = sModel;
+		
+		fprintf(logFile, "sFxName: %s\n", sFxName.Text);
+		fflush(logFile);
+		return true;
+	}
+	else
+		return false;
+}
+
+unsigned int CNWCVisualEffectOnObject__LoadBeam_eax;
+
+void __declspec( naked )  CNWCVisualEffectOnObject__LoadBeam_hookmid()
+{
+	__asm{
+		mov CNWCVisualEffectOnObject__LoadBeam_eax, eax
+		lea ecx, [esp+4h]
+		push ecx
+		push eax
+		call CNWCVisualEffectOnObject__LoadBeam_Hook
+		test eax, eax
+		jz orig
+		mov ecx, 00574188h
+		jmp ecx
+
+	orig:
+		mov eax, CNWCVisualEffectOnObject__LoadBeam_eax
+		jmp CNWCVisualEffectOnObject__LoadBeam_origmid
+	}
+}
+
 void HookFunctions()
 {
 	LONG error;
@@ -78,10 +121,14 @@ void HookFunctions()
 	DetourUpdateThread(GetCurrentThread());
 
 	*(DWORD*)&CNWCItem__LoadVisualEffect_origmid = 0x004EA0EA;
+	*(DWORD*)&CNWCVisualEffectOnObject__LoadBeam_origmid = 0x00574112;
 	//*(DWORD*)&CNWCItem__LoadVisualEffect_origmid = 0x004EA40B;
 	
 	error = DetourAttach(&(PVOID&)CNWCItem__LoadVisualEffect_origmid, CNWCItem__LoadVisualEffect_hookmid);
 	fprintf(logFile, "CNWCItem::LoadVisualEffect hook: %d\n", error);
+
+	error = DetourAttach(&(PVOID&)CNWCVisualEffectOnObject__LoadBeam_origmid, CNWCVisualEffectOnObject__LoadBeam_hookmid);
+	fprintf(logFile, "CNWCVisualEffectOnObject::LoadBeam hook: %d\n", error);
 
 	error = DetourTransactionCommit();
 	if(error == NO_ERROR)
@@ -96,8 +143,8 @@ void InitPlugin()
 {
 	//DebugBreak();
 	logFile = fopen(logFileName, "w");
-	fprintf(logFile, "NWN Client Extender 0.1 - Tweaks plugin\n");
-	fprintf(logFile, "(c) 2011 by virusman\n");
+	fprintf(logFile, "NWN Client Extender - Tweaks plugin v.0.2.2\n");
+	fprintf(logFile, "(c) 2011-2012 by virusman\n");
 	fflush(logFile);
 	PatchImage();
 	HookFunctions();
